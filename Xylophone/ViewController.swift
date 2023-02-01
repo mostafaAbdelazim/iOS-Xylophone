c8aab5d779ae717305c03567ec3efc5af057671a
mostafaabdelazim <mzeemadev@gmail.com>
2023-02-01 13:51:31 +0200

    play sound functionality

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var   player:AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+        playSound(soundNote: sender.currentTitle!)
+    }
+    
+    func playSound(soundNote:String) {
+        guard let url = Bundle.main.url(forResource: soundNote, withExtension:"wav") else {
+            return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback,mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint:
+                                        AVFileType.wav.rawValue)
+            guard let player = player else { return }
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
